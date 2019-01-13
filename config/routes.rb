aef5f54da268984e6c823e95a3a21454510e79eb
shimahikaru <i12007ushio@gmail.com>
2019-01-13 13:56:43 +0900

    “admin_project追加”

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  root to: "top#index"
+
   resources :posts
   resources :projects, param: :name, only: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
